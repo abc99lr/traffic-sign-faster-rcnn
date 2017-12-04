4d1fdc87900730fa4308fbde22d93547fbcc5185
jnie9 <31074770+jnie9@users.noreply.github.com>
2017-12-04 04:59:54 -0600

    initial commit

--- keras_frcnn/gtsdb_parser.py
+++ keras_frcnn/gtsdb_parser.py
@@ -3,70 +3,70 @@ import cv2
 import numpy as np
 
 def get_data(input_path):
-	found_bg = False
-	all_imgs = {}
+    found_bg = False
+    all_imgs = {}
 
-	classes_count = {}
+    classes_count = {}
 
-	class_mapping = {}
+    class_mapping = {}
 
-	visualise = True
-	
-	with open(input_path,'r') as f:
+    visualise = True
 
-		print('Parsing annotation files')
+    with open(input_path,'r') as f:
 
-		for line in f:
-			line_split = line.strip().split(',')
-			(filename,x1,y1,x2,y2,class_name) = line_split
+        print('Parsing annotation files')
 
-			if class_name not in classes_count:
-				classes_count[class_name] = 1
-			else:
-				classes_count[class_name] += 1
+        for line in f:
+            line_split = line.strip().split(',')
+            (filename,x1,y1,x2,y2,class_name) = line_split
 
-			if class_name not in class_mapping:
-				if class_name == 'bg' and found_bg == False:
-					print('Found class name with special name bg. Will be treated as a background region (this is usually for hard negative mining).')
-					found_bg = True
-				class_mapping[class_name] = len(class_mapping)
+            if class_name not in classes_count:
+                classes_count[class_name] = 1
+            else:
+                classes_count[class_name] += 1
 
-			if filename not in all_imgs:
-				all_imgs[filename] = {}
-				
-				filename = get_dir_path(input_path) + '/' + filename
+            if class_name not in class_mapping:
+                if class_name == 'bg' and found_bg == False:
+                    print('Found class name with special name bg. Will be treated as a background region (this is usually for hard negative mining).')
+                    found_bg = True
+                class_mapping[class_name] = len(class_mapping)
 
-				img = cv2.imread(filename)
-				(rows,cols) = img.shape[:2]
-				all_imgs[filename]['filepath'] = filename
-				all_imgs[filename]['width'] = cols
-				all_imgs[filename]['height'] = rows
-				all_imgs[filename]['bboxes'] = []
+            if filename not in all_imgs:
+                all_imgs[filename] = {}
 
-				# we use all images as training images 
-				all_imgs[filename]['imageset'] = 'train'
+                filename = get_dir_path(input_path) + '/' + filename
 
-				# if np.random.randint(0,6) > 0:
-				# 	all_imgs[filename]['imageset'] = 'trainval'
-				# else:
-				# 	all_imgs[filename]['imageset'] = 'test'
+                img = cv2.imread(filename)
+                (rows,cols) = img.shape[:2]
+                all_imgs[filename]['filepath'] = filename
+                all_imgs[filename]['width'] = cols
+                all_imgs[filename]['height'] = rows
+                all_imgs[filename]['bboxes'] = []
 
-			all_imgs[filename]['bboxes'].append({'class': class_name, 'x1': int(x1), 'x2': int(x2), 'y1': int(y1), 'y2': int(y2)})
+                # we use all images as training images
+                all_imgs[filename]['imageset'] = 'train'
+
+                # if np.random.randint(0,6) > 0:
+                # 	all_imgs[filename]['imageset'] = 'trainval'
+                # else:
+                # 	all_imgs[filename]['imageset'] = 'test'
+
+            all_imgs[filename]['bboxes'].append({'class': class_name, 'x1': int(x1), 'x2': int(x2), 'y1': int(y1), 'y2': int(y2)})
 
 
-		all_data = []
-		for key in all_imgs:
-			all_data.append(all_imgs[key])
-		
-		# make sure the bg class is last in the list
-		if found_bg:
-			if class_mapping['bg'] != len(class_mapping) - 1:
-				key_to_switch = [key for key in class_mapping.keys() if class_mapping[key] == len(class_mapping)-1][0]
-				val_to_switch = class_mapping['bg']
-				class_mapping['bg'] = len(class_mapping) - 1
-				class_mapping[key_to_switch] = val_to_switch
-		
-		return all_data, classes_count, class_mapping
+        all_data = []
+        for key in all_imgs:
+            all_data.append(all_imgs[key])
+
+        # make sure the bg class is last in the list
+        if found_bg:
+            if class_mapping['bg'] != len(class_mapping) - 1:
+                key_to_switch = [key for key in class_mapping.keys() if class_mapping[key] == len(class_mapping)-1][0]
+                val_to_switch = class_mapping['bg']
+                class_mapping['bg'] = len(class_mapping) - 1
+                class_mapping[key_to_switch] = val_to_switch
+
+        return all_data, classes_count, class_mapping
 
 def get_dir_path(input_path):
-	return os.path.dirname(input_path)
+    return os.path.dirname(input_path)
---
+++ run.sh
@@ -0,0 +1,4 @@
+rm keras_frcnn/*.pyc
+rm results_imgs/*
+python train_frcnn.py --network=simple --parser=simple --path=../dataset/PNG_train/gt.txt --output_weight_path='./model_frcnn_zfnet.hdf5'
+#python test_frcnn.py --network=simple --path=../dataset/PNG_test --config_filename=simple_config.pickle
