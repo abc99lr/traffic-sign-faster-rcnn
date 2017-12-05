1ee59f45c604d8f489fbcf654a7b2cb1898c8605
jnie9 <31074770+jnie9@users.noreply.github.com>
2017-12-05 00:52:09 -0600

    updated gitignore

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ rm results_imgs/*
 #python train_frcnn.py --network=simple --parser=simple --path=../dataset/PNG_train/gt.txt --output_weight_path='./model_frcnn_zfnet.hdf5'
 #python test_frcnn.py --network=simple --path=../dataset/PNG_test --config_filename=simple_config.pickle
 python train_frcnn.py --output_weight_path='./model_frcnn_zfnet.hdf5'
-python test_frcnn.py  --config_filename=simple_config.pickle
+#python test_frcnn.py  --config_filename=simple_config.pickle
