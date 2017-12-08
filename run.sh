f663e90f50562ee05e0b9a8765d238b89bab8e60
jnie9 <31074770+jnie9@users.noreply.github.com>
2017-12-08 15:41:47 -0600

    updated run

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ rm results_imgs/*
 #python train_frcnn.py --path=../faster-rcnn/dataset/PNG_train/gt.txt --config_filename=simple_config.pickle --output_weight_path='./model_frcnn_zfnet.hdf5'
 #python test_frcnn.py --path=../faster-rcnn/dataset/PNG_test --config_filename=simple_config.pickle
 
-python train_frcnn.py --network=zfnet --parser=simple --num_epochs=10 --epoch_length=600 --path=../faster-rcnn/dataset/PNG_train/gt.txt --config_filename=zfnet_config.pickle --output_weight_path='./model_frcnn_zfnet.hdf5'
+python train_frcnn.py --network=zfnet --parser=simple --num_epochs=1 --epoch_length=100 --path=../faster-rcnn/dataset/PNG_train/gt.txt --config_filename=zfnet_config.pickle --output_weight_path='./model_frcnn_zfnet.hdf5'
 #python test_frcnn.py --network=zfnet --path=../faster-rcnn/dataset/PNG_test --config_filename=zfnet_config.pickle
