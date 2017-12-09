0b003d3dd113d796b74c49c4a44de3b418530197
jnie9 <31074770+jnie9@users.noreply.github.com>
2017-12-09 12:19:18 -0600

    fixed bug

--- train_frcnn.py
+++ train_frcnn.py
@@ -47,7 +47,7 @@ def set_config(options):
     elif options.network == 'vgg':
         C.network = 'vgg'
         C.num_features = 512
-        C.rpn_stride = 1
+        C.rpn_stride = 16
         from keras_frcnn import vgg as nn
     else:
         print('Not a valid model')
