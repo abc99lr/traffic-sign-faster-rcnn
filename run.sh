43dd9d04851e8e7de674305cf39bcbe17b8acfcc
jnie9 <31074770+jnie9@users.noreply.github.com>
2017-12-18 15:27:40 -0600

    12131 vgg

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ rm results_imgs/*
 #python train_frcnn.py --network=fcnet --parser=simple --num_epochs=12 --epoch_length=600 --path=../faster-rcnn/dataset/PNG_train/gt.txt --config_filename=fcnet_config_48.pickle --output_weight_path='./model_frcnn_fcnet_888.hdf5'
 #python test_frcnn.py --network=fcnet --path=../faster-rcnn/dataset/PNG_train/gt.txt --config_filename=fcnet_config_48.pickle
 #python train_frcnn.py --network=zfnet --parser=simple --num_epochs=12 --epoch_length=600 --path=../faster-rcnn/dataset/PNG_train/gt.txt --config_filename=zfnet_config_48.pickle --output_weight_path='./model_frcnn_zfnet_888.hdf5'
-python test_frcnn.py --network=zfnet --path=../faster-rcnn/dataset/PNG_train/gt.txt --config_filename=zfnet_8_config.pickle
+python test_frcnn.py --network=zfnet --path=../faster-rcnn/dataset/PNG_train/gt.txt --config_filename=vgg_8_config.pickle
 #python train_frcnn.py --network=vgg --parser=simple --num_epochs=12 --epoch_length=600 --path=../faster-rcnn/dataset/PNG_train/gt.txt --config_filename=vgg_config_48.pickle --output_weight_path='./model_frcnn_vgg_888.hdf5'
 #python test_frcnn.py --network=vgg --path=../faster-rcnn/dataset/PNG_train/gt.txt --config_filename=vgg_config_8_stride.pickle
